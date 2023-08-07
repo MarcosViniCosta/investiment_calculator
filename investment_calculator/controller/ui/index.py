c1c7dfc2c46427cc5fc4b7cc94c2abc8db9e2f18
Marcos Vinicius Costa <marcos.viniciuscosta1@outlook.com>
2023-08-07 06:48:58 -0300

    feat: Inicialização do página web

--- investment_calculator/controller/ui/index.py
+++ investment_calculator/controller/ui/index.py
@@ -13,7 +13,31 @@ async def get_index(
         request: Request,
         jinja_environment: Jinja2Templates = Depends(get_jinja_environment)
 ):
+
+    dados = [
+        {
+            'moeda':'BTC',
+            'segunda': 150000.00,
+            'terca': 150000.00,
+            'quarta': 150000.00,
+            'quinta': 150000.00,
+            'sexta': 150000.00,
+            'sabado': 150000.00,
+         },
+
+        {
+            'moeda': 'dolar',
+            'segunda': 150000.00,
+            'terca': 150000.00,
+            'quarta': 150000.00,
+            'quinta': 150000.00,
+            'sexta': 150000.00,
+            'sabado': 150000.00,
+        },
+
+    ]
+
     return jinja_environment.TemplateResponse(
         'index.html',
-        {'request': request}
+        {'request': request, 'dados':dados}
     )
